58ded0e9c94812e3651690508822ebf37a329d72
Muhammad Abubakar Adamu <maadamu0007@gmail.com>
2022-09-08 07:29:10 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -11,7 +11,7 @@ int main(void)
 	char acharacter;
 	int ainteger;
 	long along;
-	long long alonglong;		
+	long long alonglong;
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
