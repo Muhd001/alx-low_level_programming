6d210bad6f675015c5811c45c66c38b9b96f392b
Muhammad Abubakar Adamu <maadamu0007@gmail.com>
2022-09-19 09:28:17 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: string input parameter
+ *
+ * Return: converted integer from string
+*/
+
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * sign);
+}
