6cd01dc91da8df84100de8282d52e571d7f8b540
Muhammad Abubakar Adamu <maadamu0007@gmail.com>
2022-09-30 10:04:29 -0700

    3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcmp - compare strings
+ *@s1: string
+ *@s2: string
+ *
+ *Return: integer e
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int w = 0;
+int e = 0;
+
+while (s1[w] == s2[w] && s1[w] != '\0')
+{
+w++;
+}
+j += s1[w] - s2[w];
+return (e);
+}
